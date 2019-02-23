25b0ccde844daf85d6bb749a8fbae8c1eb185b44
wagraphics <will@williamanthonygraphics.com>
2019-02-23 08:57:16 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
+exports.ACCESS_TOKEN = "JqEkKFxHHpzBJU4dhTu3fPJT1EYXZJ1Xv57TCQ6n";
+exports.BOT_ID = "3c250d9811e7b6992f2e8452ab";
+exports.GROUP_ID = "48425488";
 exports.GIHPY_KEY = 'Giphy API key';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
